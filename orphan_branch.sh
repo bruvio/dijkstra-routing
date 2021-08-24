5f94a98f145413db50efdebd29a5422d49f6fadd
Bruno VIOLA <bruno.viola@protonmail.com>
2021-08-24 16:51:44 +0100

    removing files

--- orphan_branch.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
- git checkout --orphan Develop
-
- git status
- git commit -am "Initial commit"
- git branch -D develop
- git branch -m develop
- git push -f origin develop
- git checkout main
