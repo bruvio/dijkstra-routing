300e5209437522c227314ac39bcb5a210b4b6b4e
Bruno VIOLA <bruno.viola@protonmail.com>
2021-08-24 14:35:08 +0100

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM python:3.8-slim
 
-ENV folder=/opt/venv
-RUN python3 -m venv $folder
-ENV PATH="$folder/bin:$PATH"
+ENV VIRTUAL_ENV=/opt/venv
+RUN python3 -m venv $VIRTUAL_ENV
+
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
 
 WORKDIR /usr/src/app
 
@@ -15,4 +16,3 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 RUN pytest
-
--- shortestpath.py
+++ shortestpath.py
@@ -1,6 +1,11 @@
 import argparse
 import logging
+import os
+import pdb
 import sys
+import time
+
+from utils import Graph
 
 logger = logging.getLogger(__name__)
 
@@ -20,7 +25,40 @@ def main(
 ):
     logger.info("creating Graph from {}".format(path))
 
-    logger.info("computing shortest path between nodes {} and {}".format(node1, node2))
+    if os.path.exists(path):
+        logger.debug("path exists")
+        logger.info("building Graph")
+        logger.debug("reading number of nodes in current Graph")
+
+        with open(path) as f:
+            num_nodes = int(f.readline().rstrip())
+            logger.debug("{} found".format(num_nodes))
+        list_tuples = []
+        with open(path, "r") as f:
+            for line in f.readlines()[num_nodes + 2 :]:
+                n1, n2, distance = (
+                    line.split()[0],
+                    line.split()[1],
+                    int(line.split()[2]),
+                )
+                list_tuples.append(tuple([n1, n2, distance]))
+        graph = Graph(list_tuples)
+        start = time.time()
+        logger.info(
+            "computing shortest path between nodes {} and {}".format(node1, node2)
+        )
+        distance = graph.dijkstra(node1, node2)
+        logger.debug("Elapsed Time: %s" % (time.time() - start))
+
+        logger.debug(
+            "distance between node {} and node {} is \n {}".format(
+                node1, node2, distance
+            )
+        )
+
+    else:
+        logger.error("specified path does not exists")
+        return
 
 
 if __name__ == "__main__":
@@ -43,7 +81,7 @@ if __name__ == "__main__":
         "--debug",
         type=int,
         help="Debug level. 0: Error, 1: Warning, 2: Info, 3: Debug, 4: Debug Plus",
-        default=1,
+        default=2,
     )
     args = parser.parse_args(sys.argv[1:])
 
@@ -62,7 +100,7 @@ if __name__ == "__main__":
 
     logger = logging.getLogger(__name__)
 
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     main(
         args.path,
         args.node1,
